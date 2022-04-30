6f496899ad4f634466fa23fef34797c8ce592705
Amnaazam123 <67511883+Amnaazam123@users.noreply.github.com>
2022-05-01 02:39:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/signuppagedesign/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.signuppagedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button register;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            username = findViewById(R.id.username);
+            password = findViewById(R.id.password);
+            register = findViewById(R.id.registration2);
+            login = findViewById(R.id.login2);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/com/example/signuppagedesign/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.signuppagedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText address;
+    EditText email;
+    Button register;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        address = findViewById(R.id.address);
+        email = findViewById(R.id.email);
+        register = findViewById(R.id.registration);
+        login = findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent LoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(LoginActivity);
+            }
+        });
+
+
+    }
+}
